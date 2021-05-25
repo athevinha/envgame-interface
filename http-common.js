a192d71bc21c082a9c5350a43f566ccc00f7fabd
pvinhp <nguyenthevinh1002@gmail.com>
2021-05-25 16:14:40 +0700

    change

--- http-common.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-export default axios.create({
-  baseURL:
-    process.env.NODE_ENV === "production"
-      ? "http://13.212.243.248:8080/api"
-      : "http://localhost:8080/api",
-});
