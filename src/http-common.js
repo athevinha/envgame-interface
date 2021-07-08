f0c9dd22485f092c077f4b4ba369ef2142256e79
pvinhp <nguyenthevinh1002@gmail.com>
2021-07-08 23:24:15 +0700

    hhehe

--- src/http-common.js
+++ src/http-common.js
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseURL:
     process.env.NODE_ENV === "production"
-      ? "http://3.0.241.73:8080/api"
+      ? "https://api.envgame.online/api"
       : "http://localhost:8080/api",
 });
