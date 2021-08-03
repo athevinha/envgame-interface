c4a4494ce794d7fed4d49f36f1542d2a2eb0c7cc
Envgame_Github <nguyenthevinh1002@gmail.com>
2021-08-03 20:16:53 +0700

    add charts game

---
+++ src/components/Game/ChartGame.component.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import UserService from "../../service/user.service";
+import "../../style/Games.css";
+import Iframe from "react-iframe";
+import { ToastContainer, toast } from "react-toastify";
+let STime = 0;
+export default class ChartGame extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      game: this.props.game,
+    };
+  }
+  componentDidMount() {
+    // if (localStorage.getItem("tooken") == null) {
+    //   this.updateForm.current.className = "update-admin display";
+  }
+  componentWillUnmount() {}
+  render() {
+    return (
+      <div className="background-iframe darker_blue">
+        <ToastContainer />
+        {/* Same as */}
+        <ToastContainer />
+        <div className="enter_game"></div>
+      </div>
+    );
+  }
+}
