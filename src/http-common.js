83a0bff01235a19380997b57a5e42f70cc949f09
Envgame_Github <nguyenthevinh1002@gmail.com>
2021-08-30 13:14:53 +0700

    ahihi

--- src/http-common.js
+++ src/http-common.js
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseURL:
     process.env.NODE_ENV === "production"
-      ? "https://api.envgame.online/api"
-      : "http://localhost:8080/api",
+      ? "https://api.envgame.online/api/envgame/conchuot2945@/2945"
+      : "http://localhost:8080/api/envgame/conchuot2945@/2945",
 });
