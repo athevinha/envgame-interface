d00791b5152692a0f9541c0a1010ec5fbe90abcb
Envgame_Github <nguyenthevinh1002@gmail.com>
2021-09-10 23:17:44 +0700

    change for hackathon

--- src/http-common.js
+++ src/http-common.js
@@ -1,8 +1,6 @@
 import axios from "axios";
-// const { SERVER_BUILD_URL, SERVER_LOCAL_URL } = process.env;
+const { SERVER_BUILD_URL, SERVER_LOCAL_URL } = process.env;
 export default axios.create({
   baseURL:
-    process.env.NODE_ENV === "production"
-      ? "https://api.envgame.online/api/envgame"
-      : "http://localhost:8080/api/envgame",
+    process.env.NODE_ENV === "production" ? SERVER_BUILD_URL : SERVER_LOCAL_URL,
 });
