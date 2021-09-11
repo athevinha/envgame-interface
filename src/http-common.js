e34cf0408e84319999a9bed9a5580ad361247a56
Envgame_Github <nguyenthevinh1002@gmail.com>
2021-09-11 12:16:48 +0700

    chage commom

--- src/http-common.js
+++ src/http-common.js
@@ -1,5 +1,6 @@
 import axios from "axios";
-const { SERVER_BUILD_URL, SERVER_LOCAL_URL } = process.env;
+const SERVER_BUILD_URL = "HIDE URL SERVER",
+  SERVER_LOCAL_URL = "http://localhost:8080/api/envgame";
 export default axios.create({
   baseURL:
     process.env.NODE_ENV === "production" ? SERVER_BUILD_URL : SERVER_LOCAL_URL,
